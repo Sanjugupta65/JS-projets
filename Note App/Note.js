ebf78d4523219080140e7b6d6d73f629c6e87dc5
Sanju <sanju3139188@gmail.com>
2026-04-01 23:39:01 +0530

    make the basic html structure

---
+++ Note App/Note.js
@@ -0,0 +1,8 @@
+const Title = document.getElementById(note-title);
+const noteItem = document.getElementById(note-body)
+const saved_things = document.getElementById(save-btn)
+const saved_notes = document.getElementById(note-container)
+
+saved_things.addEventListener('click',function(){
+    var titlevalue = saved_things.value
+})
